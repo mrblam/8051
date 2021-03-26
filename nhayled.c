fb09c0af6e03bed0dc28e79aedd2c232e2a144c9
mrblam <hp.831557@gmail.com>
2021-03-26 12:06:43 +0700

    alo

---
+++ nhayled.c
@@ -0,0 +1,18 @@
+#include <REG52.H>
+
+sbit LED_PIN = P0.0;
+void delay_ms(unsigned int t){
+    unsigned int x,y;
+    for (x = 0;x<t;x++){
+        for(y=0;y<123;y++);
+    }
+}
+ 
+ void main (){
+     while (1)
+     {
+         LED_PIN = ! LED_PIN;
+         delay_ms(500);
+     }
+     
+ }
